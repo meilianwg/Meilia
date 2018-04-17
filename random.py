f8eeb56e7e4c76e9890b3eb96064edee673cb022
Meilia Nawang Ningrum <37320112+meilianwg@users.noreply.github.com>
2018-04-17 11:02:03 +0700

    Add files via upload

---
+++ random.py
@@ -0,0 +1,8 @@
+import random
+jumlah = input ("Masukkan Jumlah N :")
+print "#########################################"
+for n in range (jumlah):
+    n = random.uniform (0.0, 0.5)
+    print ("\t"), n
+print "#########################################"
+print ("selesai")
